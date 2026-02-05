e887ca84d4939edb5db2370c78f615cbf8137236
bagdenius <bagdensparrow@gmail.com>
2026-02-05 13:34:02 +0200

    feat: enhance error handling for uncaught exceptions and unhandled rejections in server

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,27 @@ import mongoose from 'mongoose';
 
 import { app } from './app.ts';
 
+process.on('uncaughtException', (error: Error) => {
+  console.log('❌ UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(`${error.name}: ${error.message}`);
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE!.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD!,
 );
 
-await mongoose.connect(DB);
+const connection = await mongoose.connect(DB);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (error: Error) => {
+  console.log('❌ UNHANDLED REJECTION! Shutting down...');
+  console.log(error);
+  server.close(() => process.exit(1));
 });
