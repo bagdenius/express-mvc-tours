a730af49734bf4d8a62b9c3609277ec2e5891b1f
bagdenius <bagdensparrow@gmail.com>
2026-02-11 18:35:08 +0200

    build overview page with tour cards

--- src/controllers/view-controller.ts
+++ src/controllers/view-controller.ts
@@ -1,8 +1,14 @@
-import type { Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 
-export const getOverview = (request: Request, response: Response) => {
-  response.status(200).render('overview', { title: 'All tours' });
-};
+import { Tour } from '../models/tour-model.ts';
+import { catchAsync } from '../utils/catchAsync.ts';
+
+export const getOverview = catchAsync(
+  async (request: Request, response: Response, next: NextFunction) => {
+    const tours = await Tour.find();
+    response.status(200).render('overview', { title: 'All tours', tours });
+  },
+);
 
 export const getTour = (request: Request, response: Response) => {
   response.status(200).render('tour', { title: 'Tour name placeholder' });
